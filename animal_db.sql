e64a535a6794563c35c2d4960611d9ebf966a2d8
Antitect <84257488+Antitect@users.noreply.github.com>
2026-04-01 14:48:13 -0500

    Add animal_db.sql
    Basic starting database to work with; unsure how XAMPP file structure differs exactly from AMPPS so I just exported this into an sql file for now.

---
+++ animal_db.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Apr 01, 2026 at 09:38 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `animal_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Animals`
+--
+
+CREATE TABLE `Animals` (
+  `Animal_ID` int(11) NOT NULL,
+  `Name` text DEFAULT NULL,
+  `Species` text DEFAULT NULL,
+  `Breed` text DEFAULT NULL,
+  `Age` int(11) DEFAULT NULL,
+  `Adopted` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Animals`
+--
+ALTER TABLE `Animals`
+  ADD PRIMARY KEY (`Animal_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Animals`
+--
+ALTER TABLE `Animals`
+  MODIFY `Animal_ID` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
