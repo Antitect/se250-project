6a765adfe5d6aeff003317f4e1774464f1d83c52
kkanu171104 <kanukingsley543@gmail.com>
2026-04-16 22:23:58 -0500

    Add delete_animal.php for deleting an animal record

---
+++ php/delete_animal.php
@@ -0,0 +1,20 @@
+<?php
+include "db.php";
+
+header('Content-Type: application/json');
+
+$id = $_POST['Animal_ID'] ?? '';
+
+if (empty($id)) {
+    echo json_encode(["error" => "Animal_ID is required"]);
+    exit();
+}
+
+$sql = "DELETE FROM Animals WHERE Animal_ID = $id";
+
+if ($conn->query($sql)) {
+    echo json_encode(["message" => "Animal deleted successfully"]);
+} else {
+    echo json_encode(["error" => "Delete failed"]);
+}
+?>
